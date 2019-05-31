7092e3670883f544301c758162527b922f95634e
WonderMr <alexey@yandex.ru>
2019-05-31 16:47:46 +0300

    mid commit
    Уже работает, но нужно допилить

--- gobbix.go
+++ gobbix.go
@@ -1,13 +1,14 @@
  package main
 //----------------------------------------------------------------------------------------------------------------------
  import (
-     "github.com/go-ole/go-ole"
-     "github.com/go-ole/go-ole/oleutil"
+     "fmt"
      "io/ioutil"
      "log"
      "net/http"
      "net/url"
      "os"
+     "os/exec"
+     "path/filepath"
      "regexp"
      "strings"
      "time"
@@ -16,38 +17,24 @@
 var (
     re_1c_check_base                =   regexp.MustCompile("check\\savailability\\s(.*)\\severy\\s(.*)\\snotify\\s(.*)")
     re_quotes                       =   regexp.MustCompile(`\'|\"|\r|\n`)
-    //proxyURL, err                   =   url.Parse("http://110.235.207.203:3128")
-    transport                       =   &http.Transport{
-        //Proxy: http.ProxyURL(proxyURL),
-    }
+    re_str                          =   regexp.MustCompile("(?m)^(\\w+)?\\:(.+)$")
+    re_filename                     =   regexp.MustCompile(`[\\|\/]([^\\|\/]*)$`)
+    re_excp_descr                   =   regexp.MustCompile(`\:(.*)'$`)
+    re_excp                         =   regexp.MustCompile(`,EXCP,`)
+    transport                       =   &http.Transport{}
     client                          =   &http.Client{
         Transport                   :   transport,
         Timeout                     :   time.Second * 15,
     }
+    c1_client                       =   ""
+    working_dir                     =   ""
+    exit_epf                        =   ""
+    tzh_dir                         =   ""
+    logcfg_xml                      =   ""
+    conf_cfg                        =   ""
+    conf_cfg_value                  =   "DisableUnsafeActionProtection=.*"
+    bot_send_url                    =   ""
 )
-//----------------------------------------------------ё------------------------------------------------------------------
-type Block struct {
-     Try     func()
-     Catch   func(Exception)
-     Finally func()
-}
-type Exception interface{}
-    func Throw(up Exception) {
-        panic(up)
-    }
-    func (tcf Block) Do() {
-        if tcf.Finally              !=  nil {
-            defer tcf.Finally()
-        }
-        if tcf.Catch                !=  nil {
-            defer func() {
-            if r                    :=  recover(); r !=  nil {
-                tcf.Catch(r)
-            }
-        }()
-    }
-    tcf.Try()
-}
 //----------------------------------------------------------------------------------------------------------------------
 func log_it(msg string) {
     log.SetOutput(os.Stdout)
@@ -66,75 +53,137 @@ func clean_quotes(cq_str string)string{
     return re_quotes.ReplaceAllString(cq_str,"")
 }
 //----------------------------------------------------------------------------------------------------------------------
-func check_1c_database_availability(config_line string,bot_send string){
-    c1_available                    :=  false
+func check_1c_database_availability(config_line string){
+    c1_available                    :=  true
     matches                         :=  re_1c_check_base.FindAllStringSubmatch(clean_quotes(config_line),-1)
     if(len(matches)                 ==  1){
         c1_conn_str                 :=  matches[0][1]
         c1_delay,_                  :=  time.ParseDuration(matches[0][2])
         c1_send_to                  :=  matches[0][3]
+        run_1c                      :=  exec.Command(c1_client,"ENTERPRISE","/IBConnectionString"+c1_conn_str,"/DisableStartupMessages","/DisableStartupDialogs",`/execute`+exit_epf)
+        c1_exe_name                 :=  re_filename.FindStringSubmatch(c1_client)[0]
         log_it("Database = "+c1_conn_str+"; Period = "+matches[0][2]+"; Notify chat = "+c1_send_to)
-        url_not_available           :=  bot_send+c1_send_to+"&text="+url.QueryEscape("\xF0\x9F\x98\xAD database \xE2\x97\x80"+c1_conn_str+"\xE2\x96\xB6 is NOT available \xF0\x9F\x98\xB2 \xE2\x9A\xA1 \xE2\x9A\xA1 \xE2\x9A\xA1")
-        url_available               :=  bot_send+c1_send_to+"&text="+url.QueryEscape("\xF0\x9F\x98\xB8 database \xE2\x97\x80"+c1_conn_str+"\xE2\x96\xB6 available \xF0\x9F\x99\x8F \xF0\x9F\x92\xAA \xF0\x9F\x92\xAA \xF0\x9F\x92\xAA")
+        url_not_available           :=  bot_send_url+c1_send_to+"&text="+url.QueryEscape("\xF0\x9F\x98\xAD database \xE2\x97\x80"+c1_conn_str+"\xE2\x96\xB6 is NOT available \xF0\x9F\x98\xB2 \xE2\x9A\xA1 \xE2\x9A\xA1 \xE2\x9A\xA1")
+        url_available               :=  bot_send_url+c1_send_to+"&text="+url.QueryEscape("\xF0\x9F\x98\xB8 database \xE2\x97\x80"+c1_conn_str+"\xE2\x96\xB6 available \xF0\x9F\x99\x8F \xF0\x9F\x92\xAA \xF0\x9F\x92\xAA \xF0\x9F\x92\xAA")
         for {
             log_it("checking "+c1_conn_str)
-            Block{
-                Try: func() {
-                    ole.CoInitialize(0)
-                    com, _          :=  oleutil.CreateObject("V83c.Application")
-                    c1, _           :=  com.QueryInterface(ole.IID_IDispatch)
-                    oleutil.MustCallMethod(c1, "Connect", c1_conn_str).ToIDispatch()
-                    oleutil.MustCallMethod(c1, "Exit", false).ToIDispatch()
-                    if(!c1_available){
-                        client.Get(url_available)
-                        //ret, ret2   := client.Get(url_available)
-                    }
+            c1da_result             :=  run_1c.Start()
+            if(c1da_result          !=  nil){
+                log_it("process start error")
+            }
+            ret,_                   :=  os.FindProcess(run_1c.Process.Pid);
+            c1_tzh_dir              :=  tzh_dir+strings.Replace(c1_exe_name,".exe","",-1)+"_"+fmt.Sprint(run_1c.Process.Pid)
+            ret.Wait()
+            log_it("process pid="+fmt.Sprint(run_1c.Process.Pid)+" ended"+c1_exe_name)
+            log_it("checking "+c1_tzh_dir)
+            var cl_files []string
+            cl_is_exception         :=  false
+            err                     :=  filepath.Walk(c1_tzh_dir, func(path string, info os.FileInfo, err error) error {
+                cl_files            =   append(cl_files, path)
+                return nil
+            })
+            if err                  !=  nil {
+                panic(err)
+            }
+            for _, cl_file          := range cl_files {
+                log_it("processing "+cl_file)
+                cl_b, err           :=  ioutil.ReadFile(cl_file)
+                if err              !=  nil {
+                    log_it(err.Error())
+                }
+                if(re_excp.Match(cl_b)){
+                    cl_is_exception =   true
+                    log_it("Exception detected:"+string(cl_b))
+                }
+                os.Remove(cl_file)
+            }
+            os.Remove(c1_tzh_dir)
+            if(cl_is_exception){
+                if(c1_available) {
+                    c1_available    =   false
+                    client.Get(url_not_available)
+                }
+            }else{
+                if(!c1_available) {
                     c1_available    =   true
-                    log_it("Available " +c1_conn_str)
-                },
-                Catch: func(e Exception) {
-                    log_it("UnAvailable "+c1_conn_str)
-                    if(c1_available) {
-                        client.Get(url_not_available)
-                        //ret, ret2   := client.Get(url_not_available)
-                        //fmt.Printf("Caught1 %v\n", ret)
-                        //fmt.Printf("Caught2 %v\n", ret2)
-                        c1_available=   false
-                    }
-                },
-                Finally: func() {
-                    time.Sleep(10*time.Second)
-                    ole.CoUninitialize()
-                },
-            }.Do()
+                    client.Get(url_available)
+                }
+            }
             time.Sleep(c1_delay)
         }
     }
 }
 //----------------------------------------------------------------------------------------------------------------------
-func main() {
-	conf_file                       :=  strings.Replace(os.Args[0], ".exe", ".config", 1)
-    re_str                          :=  regexp.MustCompile("(?m)^(\\w+)?\\:(.+)$")
-    if(strings.Contains(os.Args[0],"go_build")){
-        conf_file                   =  "c:/repos/gobbix/gobbix.conf"                                                      // debug shit
+func check_logcfg_xml(clx_1c_exe string){
+    clx_rewrite                     :=  false                                                                           //перезаписывать ли logcfg.xml
+    clx_1c_filename                 :=  re_filename.FindAllString(clx_1c_exe,-1)[0]                                 //имя исполняемого файла 1с
+    clx_logcfg_xml                  :=  strings.Replace(clx_1c_exe,clx_1c_filename,"",-1)+"/conf/logcfg.xml"   //путь к logcfg.xml
+    log_it("chechking "+clx_logcfg_xml)
+    if _, err                       :=  os.Stat(clx_logcfg_xml); os.IsNotExist(err) {
+        log_it(clx_logcfg_xml+" not exists")
+        clx_rewrite                 =   true
+    }else{
+        bytes, err                  :=  ioutil.ReadFile(clx_logcfg_xml)
+        if err                      !=  nil {
+            log_it(err.Error())
+            os.Exit(-1)
+        }
+        if(string(bytes)            !=  logcfg_xml){
+            log_it(clx_logcfg_xml+" incorrect")
+            clx_time                :=  time.Now()
+            clx_time_string         :=  clx_time.Format("2006-01-02 15-04-05")
+            os.Rename(clx_logcfg_xml,clx_logcfg_xml+"."+clx_time_string)
+            clx_rewrite             =   true
+        }
     }
+    if(clx_rewrite) {
+        log_it("writing "+clx_logcfg_xml)
+        clx_fo, err                 :=  os.Create(clx_logcfg_xml)
+        if err                      !=  nil {
+            log_it(err.Error())
+            os.Exit(-1)
+        }
+        clx_fo.WriteString(logcfg_xml)
+        clx_fo.Close()
+    }
+}
+//----------------------------------------------------------------------------------------------------------------------
+func main() {
+    //set global vars~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    working_dir,_                   =   os.Getwd()
+    working_dir                     =   strings.Replace(working_dir,"\\","/",-1)
+    conf_file                       :=  working_dir+"/gobbix.conf"
+    tzh_dir                         =   working_dir+"/tzh"
+    logcfg_xml                      =
+`<config xmlns="http://v8.1c.ru/v8/tech-log">
+    <log location="`+tzh_dir +`" history="1">
+        <event>
+            <eq property="name" value="EXCP"/>
+        </event>
+        <property name="all"/>
+    </log>
+</config>`
+    exit_epf                        =   working_dir+"/exit.epf"
+    //read & process config~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     log_it("processing "+conf_file)
     bytes, err                      :=  ioutil.ReadFile(conf_file)
     if err                          !=  nil {
         log_it(err.Error())
         os.Exit(-1)
     }
-    bot_send                        :=  ""
     for _, record                   :=  range re_str.FindAllSubmatch(bytes,-1) {
         key                         :=  strings.ToUpper(string(record[1]))
         value                       :=  string(record[2])
         switch key {
         case "TELEGRAM_TOKEN":
             log_it("telegram_token is "+clean_quotes(value))
-            bot_send                =   "https://api.telegram.org/bot"+clean_quotes(value)+"/sendMessage?chat_id="
+            bot_send_url            =   "https://api.telegram.org/bot"+clean_quotes(value)+"/sendMessage?chat_id="
             break
         case "WMI":
             log_it(value)
+            break
+        case "1C_CLIENT":
+            c1_client               =   strings.Replace(clean_quotes(value),"\\","/",-1)
             break
         case "PROXY":
             proxyURL,_              :=  url.Parse(clean_quotes(value))
@@ -145,8 +194,9 @@ func main() {
             break
         case "1C":
             if(re_1c_check_base.MatchString(value)){
+                check_logcfg_xml(c1_client)
                 log_it("processing rule "+value)
-                go check_1c_database_availability(value,bot_send);
+                go check_1c_database_availability(value);
             }
             break
         }
